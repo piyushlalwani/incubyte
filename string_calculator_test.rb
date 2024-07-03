08c090536f2b5bc09d86e36cc27c255667d703d3
Piyush Lalwani <piyushlalwani143@gmail.com>
2024-07-03 18:26:41 +0530

    Adding the basic logic for handling the direct case for 0,1,2 numbers

--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -2,18 +2,19 @@ require 'minitest/autorun'
 require_relative 'string_calculator'
 
 class StringCalculatorTest < Minitest::Test
+  def setup
+    @calculator = StringCalculator.new
+  end
+
   def test_empty_string
-    calculator = StringCalculator.new
-    assert_equal 0, calculator.add("")
+    assert_equal 0, @calculator.add("")
   end
 
   def test_single_number
-    calculator = StringCalculator.new
-    assert_equal 1, calculator.add("1")
+    assert_equal 1, @calculator.add("1")
   end
 
   def test_two_numbers
-    calculator = StringCalculator.new
-    assert_equal 6, calculator.add("1,5")
+    assert_equal 6, @calculator.add("1,5")
   end
 end
